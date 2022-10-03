76975e6ff07e6b2d34bfb94af452e874ffa7c34d
Vianney Gremmel <vianney.gremmel@getalma.eu>
2022-10-03 16:13:30 +0200

    Better pr_format

--- bin/bin/pr_format.py
+++ bin/bin/pr_format.py
@@ -1,13 +1,16 @@
 #!/home/vianney/.pr_format/bin/python
 """
 Transform json output of
-gh pr list --json number,title,headRefName,author
+`gh pr list --json number,title,headRefName,author`
 into fzf input
+
+e.g:
+    create a alias or function in your shell conf to quickly checkout a pr
+    `gh pr list --json number,title,headRefName,author --limit 500 | pr_format.py | fzf | cut -f1 -d" " | xargs gh pr checkout`
 """
 import json
 import sys
 
-from rich import box
 from rich.console import Console
 from rich.table import Table
 
@@ -17,14 +20,14 @@ def main():
     table = Table(
         show_header=False,
         show_edge=False,
-        expand=False,
+        expand=True,
         pad_edge=False,
-        box=box.SIMPLE,
+        box=None,
     )
-    table.add_column("Number", no_wrap=True, min_width=7)
+    table.add_column("Number", no_wrap=True, min_width=5)
     table.add_column("title", no_wrap=True)
-    table.add_column("author", no_wrap=True)
-    table.add_column("branch", no_wrap=True)
+    table.add_column("author")
+    table.add_column("branch")
     prs = json.load(sys.stdin)
     for pr in prs:
         table.add_row(
