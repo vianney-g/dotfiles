9078f8f00b20eb86ba01bb1585b9f53566449b8e
Vianney Gremmel <vianney.gremmel@getalma.eu>
2023-05-09 17:09:51 +0200

    Add fugitive

--- nvim/.config/nvim/lua/custom/mappings.lua
+++ nvim/.config/nvim/lua/custom/mappings.lua
@@ -8,6 +8,8 @@ M.general = {
     ["N"] = { "Nzzzv", "repeat the latest / or ? in opposite direction", opts = { nowait = true } },
     ["<C-d>"] = { "<C-d>zz", "Scroll window downward in the buffer", opts = { nowait = true } },
     ["<C-u>"] = { "<C-u>zz", "Scroll window upward in the buffer", opts = { nowait = true } },
+    ["gB"] = { "<cmd> G blame<CR>", "Blame file", opts = { nowait = true } },
+    ["gS"] = { "<cmd> G <CR>", "Fugitive status", opts = { nowait = true } },
     -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
   },
 }
--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -60,9 +60,22 @@ local plugins = {
 
   -- git
   {
-    "dinhhuy258/git.nvim",
-    config = function()
-      require("git").setup()
+    "tpope/vim-fugitive",
+    ft = { "gitcommit", "diff" },
+    init = function()
+      -- load vim-fugitive only when a git file is opened
+      vim.api.nvim_create_autocmd({ "BufRead" }, {
+        group = vim.api.nvim_create_augroup("FugitiveLazyLoad", { clear = true }),
+        callback = function()
+          vim.fn.system("git -C " .. '"' .. vim.fn.expand "%:p:h" .. '"' .. " rev-parse")
+          if vim.v.shell_error == 0 then
+            vim.api.nvim_del_augroup_by_name "FugitiveLazyLoad"
+            vim.schedule(function()
+              require("lazy").load { plugins = { "vim-fugitive" } }
+            end)
+          end
+        end,
+      })
     end,
   },
   --
