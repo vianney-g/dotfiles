7ce1b98d2004b4244da7adc3b1d6cde9c2494670
Vianney Gremmel <vianney.gremmel@getalma.eu>
2024-01-26 10:46:05 +0100

    Remove useless plugin

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,7 +1,6 @@
 -- Git related plugins
 return {
   'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
