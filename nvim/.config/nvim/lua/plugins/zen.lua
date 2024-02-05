0ae96cf242c75d7ca758923af92c954e95a6c319
Vianney Gremmel <vianney.gremmel@getalma.eu>
2024-02-05 15:36:38 +0100

    Add zen mode

---
+++ nvim/.config/nvim/lua/plugins/zen.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      plugins = {
+        tmux = { enabled = true },
+        alacritty = { enabled = true, font = "14" },
+      }
+    }
+  },
+  {
+    {
+      "folke/twilight.nvim",
+      opts = {
+      }
+    }
+  }
+}
