8d6e23c901aca49fefb93c81fdb34fd314156f13
Vianney Gremmel <vianney.gremmel@getalma.eu>
2024-03-21 15:05:58 +0100

    Add octo.vim plugin (Github integration)

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -48,4 +48,17 @@ return {
       "nvim-lua/plenary.nvim",
     },
   },
+
+  -- edit github PR from inside vim
+  {
+    "pwntester/octo.nvim",
+    config = function()
+      require "octo".setup()
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+  }
 }
