29b80748255b6a9458b32b87c30fc14a88e67d8a
Vianney Gremmel <vianney.gremmel@getalma.eu>
2024-05-27 12:02:06 +0200

    Add wezterm conf

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,36 @@
+-- Pull in the wezterm API
+local wezterm = require "wezterm"
+
+-- This table will hold the configuration
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Catppuccin Mocha'
+config.font = wezterm.font "JetBrainsMono Nerd Font"
+config.window_background_opacity = 0.75
+config.font_size = 10.0
+config.enable_tab_bar = false
+config.window_decorations = "NONE"
+
+config.keys = {
+	{
+		key = "h",
+		mods = "ALT",
+		action = wezterm.action.DecreaseFontSize,
+	},
+	{
+		key = "f",
+		mods = "ALT",
+		action = wezterm.action.IncreaseFontSize,
+	},
+}
+-- and finally, return the configuration to wezterm
+return config
