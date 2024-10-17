5bdfdd5cacc838d769fee411ddd19c1e3455ef86
Vianney Gremmel <vianney.gremmel@getalma.eu>
2024-10-17 16:28:32 +0200

    Set vim lualine style from rounded to slanted

--- nvim/.config/nvim/lua/plugins/styling.lua
+++ nvim/.config/nvim/lua/plugins/styling.lua
@@ -18,8 +18,8 @@ return {
         theme = 'onedark',
         -- component_separators = '|',
         -- section_separators = '',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
       },
       sections = {
         lualine_b = { 'branch', 'diff', 'diagnostics' },
