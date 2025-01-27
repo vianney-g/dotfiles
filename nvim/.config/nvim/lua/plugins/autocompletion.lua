dea59790cfe42c3095c23ba0f914e85b46aead00
Vianney Gremmel <vianney.gremmel@getalma.eu>
2025-01-27 10:23:18 +0100

    Let nvim default completion for command line

--- nvim/.config/nvim/lua/plugins/autocompletion.lua
+++ nvim/.config/nvim/lua/plugins/autocompletion.lua
@@ -73,13 +73,12 @@ return {
         -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'mono'
       },
-
       -- default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
         -- optionally disable cmdline completions
-        -- cmdline = {},
+        cmdline = {},
       },
 
       -- experimental signature help support
