e84004f5fb888c1789f7761554db7ba1ead88d92
Vianney Gremmel <vianney.gremmel@getalma.eu>
2025-06-23 15:20:34 +0200

    Remove deprecated mason config

--- nvim/.config/nvim/lua/plugins/code.lua
+++ nvim/.config/nvim/lua/plugins/code.lua
@@ -62,38 +62,17 @@ return {
       servers = servers,
     },
     config = function(_, opts)
-        -- mason-lspconfig requires that these setup functions are called in this order
-        -- before setting up the servers.
-        require('mason').setup()
-        require('mason-lspconfig').setup()
+      -- mason-lspconfig requires that these setup functions are called in this order
+      -- before setting up the servers.
+      require('mason').setup()
+      require('mason-lspconfig').setup()
 
-        -- Enable the following language servers
-        --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-        --
-        --  Add any additional override configuration in the following tables. They will be passed to
-        --  the `settings` field of the server config. You must look up that documentation yourself.
-        --
-        --  If you want to override the default filetypes that your language server will attach to you can
-        --  define the property 'filetypes' to the map in question.
+      -- Ensure the servers above are installed
+      local mason_lspconfig = require 'mason-lspconfig'
 
-
-        -- Ensure the servers above are installed
-        local mason_lspconfig = require 'mason-lspconfig'
-
-        mason_lspconfig.setup {
-          ensure_installed = vim.tbl_keys(servers),
-        }
-
-        mason_lspconfig.setup_handlers {
-          function(server_name)
-            require('lspconfig')[server_name].setup {
-              capabilities = require('blink.cmp').get_lsp_capabilities(),
-              on_attach = on_attach,
-              settings = servers[server_name],
-              filetypes = (servers[server_name] or {}).filetypes,
-            }
-          end,
-        }
+      mason_lspconfig.setup {
+        ensure_installed = vim.tbl_keys(servers),
+      }
     end
   },
 }
