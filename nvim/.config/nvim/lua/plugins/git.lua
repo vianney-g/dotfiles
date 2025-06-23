9815956a74bdc2b16080d2b9f5cb22d5f2a55b6e
Vianney Gremmel <vianney.gremmel@getalma.eu>
2025-06-23 15:21:28 +0200

    Lazy breaking change for gitsigns config

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -4,47 +4,43 @@ return {
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
-        -- don't override the built-in and fugitive keymaps
-        local gs = package.loaded.gitsigns
-        vim.keymap.set({ 'n', 'v' }, 'gh', function()
-          if vim.wo.diff then
-            return 'gh'
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
-        vim.keymap.set({ 'n', 'v' }, 'gH', function()
-          if vim.wo.diff then
-            return 'gH'
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
-      end,
-    },
+    config = function()
+      require('gitsigns').setup {
+        signs = {
+          add          = { text = '' },
+          change       = { text = '┃' },
+          delete       = { text = '' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        on_attach = function(bufnr)
+          -- don't override the built-in and fugitive keymaps
+          local gs = package.loaded.gitsigns
+          vim.keymap.set({ 'n', 'v' }, 'gh', function()
+            if vim.wo.diff then
+              return 'gh'
+            end
+            vim.schedule(function()
+              gs.next_hunk()
+            end)
+            return '<Ignore>'
+          end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
+          vim.keymap.set({ 'n', 'v' }, 'gH', function()
+            if vim.wo.diff then
+              return 'gH'
+            end
+            vim.schedule(function()
+              gs.prev_hunk()
+            end)
+            return '<Ignore>'
+          end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+        end,
+      }
+    end,
   },
 
-  -- open lazygit from within neovim
-  {
-    "kdheepak/lazygit.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
+  'tpope/vim-rhubarb',
 
   -- edit github PR from inside vim
   {
